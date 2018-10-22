34cf93690beff7f176560de71602b4231fadb5d8
Jess Archer <jess@jessarcher.com>
2018-10-22 22:12:38 +1000

    Add some routes for testing functionality

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,47 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/database', function () {
+    App\User::updateOrCreate(
+        [
+            'id' => 1,
+        ],
+        [
+            'name' => 'Jess',
+            'email' => 'jess@example.com',
+            'password' => bcrypt(str_random(32)),
+        ]
+    );
+
+    dump(
+        App\User::all()
+    );
+});
+
+Route::get('/cache', function () {
+    $users = Cache::remember('users', $minutes = 1, function () {
+        dump('fetching users from DB...');
+
+        return App\User::all();
+    });
+
+    dump($users);
+
+    $id = 1;
+
+    $user = Cache::remember('user:' . $id, $minutes = 1, function () use ($id) {
+        dump('fetching user from DB...');
+
+        return App\User::findOrFail($id);
+    });
+
+    dump($user);
+});
+
+Route::get('/cache/forget', function () {
+    dump(
+        Cache::forget('user:' . 1),
+        Cache::forget('users')
+    );
+});
